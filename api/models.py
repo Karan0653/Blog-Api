8853d1f63b7d0ce17440d0681f363915aba30c92
Karan0653 <karanlevi007@gmail.com>
2024-05-27 23:28:23 +0530

    migration commit

--- api/models.py
+++ api/models.py
@@ -7,6 +7,7 @@ class Author(models.Model):
     name = models.CharField(max_length=200, null=True)
     description = models.CharField(max_length = 200, null = True)
 
+
     def __str__(self) -> str:
         return self.name
     
