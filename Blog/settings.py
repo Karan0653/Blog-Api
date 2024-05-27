3ee5581847b6f768a172f9914c59aa1b54a2c8cb
Karan0653 <karanlevi007@gmail.com>
2024-05-28 00:02:58 +0530

    modified allowed hosts

--- Blog/settings.py
+++ Blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--ut((r_je+x$g)feaj4lc##nsyj4-qtpwlqnx6($&!m@eqr%ib
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
